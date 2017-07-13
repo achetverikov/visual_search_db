43b77e0b2b839d16313034aa74a6c7a5eae08950
achetverikov <stop.psy@gmail.com>
2017-07-13 21:24:37 +0200

    cleaned up

--- import_data/connect_to_neo4j.R
+++ import_data/connect_to_neo4j.R
@@ -1,8 +1,8 @@
 library(RNeo4j)
 
 graph = startGraph("http://localhost:7474/db/data/",username = 'neo4j', password = 'neo4jpass')
-data_files <- list.files(data_dir,pattern='exp.*', full.names = T)
 
 # This directory is a subdirectory of neo4j db. It should be create before import. 
 neo4j_import = '~/Neo4j/default.graphdb/import/'
 dir.create(neo4j_import, showWarnings = FALSE)
+
